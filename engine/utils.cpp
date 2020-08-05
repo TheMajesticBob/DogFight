356c040b88068dbe627e173874c8a03a4ebc5b4c
Robert Ślepecki <sir.robert@theknightsofunity.com>
2020-08-05 21:16:58 +0200

    Added scale handling in WorldToScreen & ScreenToWorld methods

--- engine/utils.cpp
+++ engine/utils.cpp
@@ -8,10 +8,9 @@ sf::Vector2f Utils::WorldToScreen(const sf::Vector2f& position)
 	
 	sf::Vector2f viewCenter = mainView.getCenter();
 	sf::Vector2f viewPosition = sf::Vector2f((viewCenter + mainView.getSize() / 2.0f).x, (viewCenter + mainView.getSize() / 2.0f).y);
-
 	sf::Vector2f viewScale = sf::Vector2f(mainView.getSize().x / rw.getSize().x, -mainView.getSize().y / rw.getSize().y);
-	//sf::Vector2f viewPosition = sf::Vector2f((mainView.getCenter() + mainView.getSize() / 2.0f).x, (mainView.getCenter() + mainView.getSize() / 2.0f).y);
-	return viewPosition - (viewCenter*2.0f) + position;
+
+	return (viewPosition - (viewCenter*2.0f) + position) / viewScale.x;
 }
 
 sf::Vector2f Utils::ScreenToWorld(const sf::Vector2f& position)
@@ -21,7 +20,7 @@ sf::Vector2f Utils::ScreenToWorld(const sf::Vector2f& position)
 
 	sf::Vector2f viewCenter = mainView.getCenter();
 	sf::Vector2f viewPosition = sf::Vector2f((viewCenter + mainView.getSize() / 2.0f).x, (viewCenter + mainView.getSize() / 2.0f).y);
-	return position - (viewPosition - viewCenter * 2.0f);
-
 	sf::Vector2f viewScale = sf::Vector2f(mainView.getSize().x / rw.getSize().x, -mainView.getSize().y / rw.getSize().y);
+
+	return (position - (viewPosition - viewCenter * 2.0f)) * viewScale.x;
 }
