a2b0878ea82cca64dac7d3eabfa6759d8ca72b88
Robert Ślepecki <sir.robert@theknightsofunity.com>
2020-08-07 19:38:36 +0200

    Updated WorldToScreen method

--- engine/utils.cpp
+++ engine/utils.cpp
@@ -6,12 +6,13 @@ sf::Vector2f Utils::WorldToScreen(const sf::Vector2f& position)
 	sf::RenderWindow& rw = Engine::GetWindow();
 	const sf::View& mainView = rw.getView(); 
 
-	sf::Vector2f viewCenter = mainView.getCenter();
-	sf::Vector2f viewPosition = sf::Vector2f((viewCenter + mainView.getSize() / 2.0f).x, (viewCenter + mainView.getSize() / 2.0f).y);
-
 	float viewScale = mainView.getSize().x / rw.getSize().x;
+	sf::Vector2f viewCenter = mainView.getCenter();
+	sf::Vector2f screenCenter = (sf::Vector2f(rw.getSize()) / 2.0f);
 
-	return (viewPosition - (viewCenter*2.0f) + position) / viewScale;
+	sf::Vector2f worldToScreenOffset = ((position - viewCenter) / viewScale + screenCenter);
+	
+	return worldToScreenOffset;
 }
 
 sf::Vector2f Utils::ScreenToWorld(const sf::Vector2f& position)
@@ -25,5 +26,5 @@ sf::Vector2f Utils::ScreenToWorld(const sf::Vector2f& position)
 	
 	sf::Vector2f mouseToCenterOffset = (position - screenCenter) * viewScale;
 
-	return (viewCenter + mouseToCenterOffset);
+	return viewCenter + mouseToCenterOffset;
 }
